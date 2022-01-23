49192befdf629222480ad111f3bcced836b12a2d
Shivang Medhekar <shivangmedhekar@gmail.com>
2022-01-23 15:49:29 -0500

    added owl carousel

---
+++ public/js/owlcarousel-config.js
@@ -0,0 +1,35 @@
+// $(document).ready(function(){
+//     $(".top-movies-carousel").owlCarousel({
+//         autoplay: true,
+//         autoplayhoverpause: true,
+//         autoplaytimeout: 1000,
+//         items: 3,
+//         loop: true,
+//         lazyLoad: true,
+//
+//         responsive: {
+//
+//             0: {
+//                 item: 1
+//             },
+//             485: {
+//                 items: 1
+//             },
+//             728: {
+//                 items: 2
+//             },
+//             960: {
+//                 items: 2
+//             },
+//             1200: {
+//                 items: 2
+//             },
+//             1900: {
+//                 items: 4
+//             }
+//
+//         }
+//     });
+//
+// });
+//

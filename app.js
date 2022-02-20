124b6e97d23403bb427e6da11385578a4bc36cff
Shivang Medhekar <shivangmedhekar@gmail.com>
2022-02-19 21:54:36 -0500

    added middleware for get /profile

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ require('dotenv').config();
 const app = express();
 const configRoutes = require('./routes');
 app.use(express.json());
+const session = require('express-session');
+
+app.use(
+    session({
+        name: 'CMA',
+        secret: "This is a secret.. shhh don't tell anyone",
+        saveUninitialized: true,
+        resave: false,
+        cookie: { maxAge: 3600000*24 }
+    })
+);
+
+app.get('/profile', (req, res, next) => {
+    if (!req.session.user) {
+        return res.status(401).render('pages/error/error', {error: "You need to be logged in"});
+    } else {
+        next();
+    }
+});
 
 const static = express.static(__dirname + '/public');
 

ad7ccb015325e62176c1478d618c7358de5cd8fc
Shivang Medhekar <shivangmedhekar@gmail.com>
2022-02-19 22:00:05 -0500

    changed location of home.js client side file

--- public/js/pages/home/home.js
+++ public/js/pages/home/home.js
@@ -1,29 +1,37 @@
 
 (function ($) {
-
+    $('#latest-releases-list').hide();
+    $('#now-playing-list').hide();
+    $('#upcoming-movies-list').hide();
     for (let movie of latestReleases){
         $('#latest-releases-list').append(
             `<a href="/movie/${movie.slug}">
                 <li class="item">
                     <div class="latest-releases-showcase">
-                        <img src="https://image.tmdb.org/t/p/original/${movie.backdrop}"/>
+                        <img src="${movie.backdrop}"/>
                     </div>
                 </li>
             </a>`)
     }
 
     for (let movie of nowPlaying){
+        let subString = [];
+        if (movie.genre) subString.push(movie.genre);
+        if (movie.runTime) subString.push(`${movie.runTime} mins`);
+        if (movie.mpaaRating) subString.push(movie.mpaaRating);
+
+        const movieSubInfo = subString.join(' | ')
         $('#now-playing-list').append(
             `<a href="/movie/${movie.slug}">
                 <li class="item">
                     <div class="now-playing-box">
                         <div class="now-playing-box-img">
-                            <img src="https://image.tmdb.org/t/p/original/${movie.poster}">
+                            <img src="${movie.poster}">
                         </div>
         
                         <div class="now-playing-box-text">
-                            <h6>${movie.movieName.replace('/qoute', "'")}</h6>
-                            <p>${movie.genre} | ${movie.runTime} min | ${movie.mpaaRating}</p>
+                            <h6>${movie.movieName.replace('/quote', "'")}</h6>
+                            <p>${movieSubInfo}</p>
                         </div>
                     </div>
                 </li>
@@ -37,11 +45,11 @@
                 <li class="item">
                     <div class="now-playing-box">
                         <div class="now-playing-box-img">
-                            <img src="https://image.tmdb.org/t/p/original/${movie.poster}">
+                            <img src="${movie.poster}">
                         </div>
         
                         <div class="now-playing-box-text">
-                            <h6>${movie.movieName.replace('/qoute', "'")}</h6>
+                            <h6>${movie.movieName.replace('/quote', "'")}</h6>
                             <p>${(new Date(movie.releaseDate)).toLocaleDateString() }</p>
                         </div>
                     </div>
@@ -50,4 +58,19 @@
     }
 
 
+    $('#latest-releases-list').show();
+    $('#now-playing-list').show();
+    $('#upcoming-movies-list').show();
+
 })(window.jQuery);
+
+$(document).ready(function() {
+    $('#latest-releases-list, #now-playing-list, #upcoming-movies-list').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#latest-releases-list, #now-playing-list, #upcoming-movies-list').removeClass('cS-hidden');
+        }
+    });
+
+});

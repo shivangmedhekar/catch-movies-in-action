d315e6633520ce68435180cf2a74bff309797a53
Shivang Medhekar <shivangmedhekar@gmail.com>
2022-02-19 21:58:28 -0500

    Changed /profile route from homeRoutes to userRoutes

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const seatSelectionRoutes = require('./seatselection');
 const showtimesRoutes = require('./showtimes');
 const theaterRoutes = require('./theater');
 const checkoutRoutes = require('./checkout');
+const userRoutes = require('./users');
 
 const constructorMethod = (app) => {
 
     app.use('/', homeRoutes);
+    app.use('/profile', userRoutes);
     app.use('/movie', movieRoutes);
     app.use('/showtimes', showtimesRoutes);
     app.use('/seatselection', seatSelectionRoutes);

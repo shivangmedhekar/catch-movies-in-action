5a48ca8c7a52e3dd793d5bc0b77dffbbb81b296a
Shivang Medhekar <shivangmedhekar@gmail.com>
2022-02-19 21:58:52 -0500

    Added Authentication and Authorization routes

--- routes/home.js
+++ routes/home.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const movieData = require('./../data/movies');
+const usersData = require('./../data/users');
+const showtimeData = require('./../data/showtimes');
 
 router.get('/', async (req, res) => {
 
     // Upcoming Movies and Top Rated Movies
     try {
-        const [nowPlayingMovies, latestReleases] = await movieData.getNowPlayingMovies(15);
-        const upcomingMovies = await movieData.getUpcomingMovies(15);
+        await showtimeData.clearOldShowtime();
+        const [nowPlayingMovies, latestReleases] = await movieData.getMovies(15, 'now-playing', true);
+        const upcomingMovies = await movieData.getMovies(15, 'coming-soon', false);
 
         res.status(200).render('pages/home/home.handlebars', {
             nowPlayingMovies: JSON.stringify(nowPlayingMovies), upcomingMovies: JSON.stringify(upcomingMovies),
@@ -24,7 +27,7 @@ router.get('/nowplaying', async (req, res) => {
 
     // Upcoming Movies and Top Rated Movies
     try {
-        const [nowPlayingMovies, latestReleases] = await movieData.getNowPlayingMovies(30);
+        const nowPlayingMovies = await movieData.getMovies(30, 'now-playing', false);
 
         res.status(200).render('pages/home/nowplaying.handlebars',
             {nowPlayingMovies: JSON.stringify(nowPlayingMovies)})
@@ -39,7 +42,7 @@ router.get('/upcoming', async (req, res) => {
 
     // Upcoming Movies and Top Rated Movies
     try {
-        const upcomingMovies = await movieData.getUpcomingMovies(30);
+        const upcomingMovies = await movieData.getMovies(30, 'coming-soon', false);
 
         res.status(200).render('pages/home/upcoming.handlebars',
             {upcomingMovies: JSON.stringify(upcomingMovies)})
@@ -47,6 +50,72 @@ router.get('/upcoming', async (req, res) => {
     }catch (e) {
         console.log(e)
         res.status(503).render('pages/error/error', {error: "Error 503: Internal Server Error"})
+    }
+});
+
+router.post('/signup', async (req, res) => {
+
+    try {
+        const { firstName, lastName, email, phoneNo, dob, password, confirmPassword } = req.body;
+
+
+        const signUpInfo = await usersData.createUser( firstName, lastName, email, phoneNo, dob, password, confirmPassword );
+
+        if (signUpInfo.userInserted) res.status(200).json(signUpInfo.user);
+
+    }catch (e) {
+        res.status(400).json({'error': e});
+    }
+
+});
+
+router.get('/logout', async (req, res) => {
+
+    if (req.session.user){
+        req.session.destroy();
+        res.status(200).json({'Logged Out': "Successfully"});
+    }
+    else {
+        res.status(204).json();
+    }
+
+});
+
+// router.get('/profile', async (req, res) => {
+//
+//     try{
+//         if (! req.session.user) throw 'Error: You should be logged in to acess this page';
+//
+//         res.render('pages/users/user', {user: req.session.user})
+//     }catch (e){
+//
+//     }
+// });
+
+router.get('/auth-status', async (req, res) => {
+
+    if(req.session.user) return res.json({ 'authStatus': true});
+    else return res.json({ 'authStatus': false});
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const loginInfo = await usersData.checkUser(email, password);
+
+        if (loginInfo.authenticated) {
+            req.session.user = {
+                userId: loginInfo.user._id,
+                email: loginInfo.user.email,
+                firstName: loginInfo.user.firstName,
+                lastName: loginInfo.user.lastName
+            }
+
+            res.status(200).json(loginInfo.user);
+        }
+    }catch (e) {
+        res.status(400).json({'error': e});
     }
 });
 module.exports = router;

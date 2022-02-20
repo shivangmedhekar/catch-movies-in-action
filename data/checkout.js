1573e07301e129243caffd161890a20cdc7ffc1e
Shivang Medhekar <shivangmedhekar@gmail.com>
2022-02-19 21:57:45 -0500

    Update checkout.js

--- data/checkout.js
+++ data/checkout.js
@@ -2,18 +2,37 @@ const mongoCollections = require('./../config/mongoCollections');
 const orders = mongoCollections.orders;
 const showtimes = mongoCollections.showtimes;
 
-async function checkout(orderSummary){
+async function checkout(orderSummary, user){
 
     const ordersCollection = await orders();
+    orderSummary.userId = user.userId;
+    if (! await checkSeatsAvailability(orderSummary.showtimeId, orderSummary.seats)) throw 'Error: Seats Unavailable'
+
     let orderInsert = await ordersCollection.insertOne(orderSummary);
-    await updateMovieAvailability(orderSummary.showtimeId, orderSummary.seats)
-    return
+
+    await updateMovieAvailability(orderSummary.showtimeId, orderSummary.seats);
+
+    return orderSummary;
+}
+
+
+
+async function checkSeatsAvailability(showtimeId, seats){
+
+    const showtimesCollection = await showtimes();
+
+    let showtimeObj = await showtimesCollection.findOne({showtimeId: showtimeId});
+    console.log(showtimeObj);
+    for (let seat of seats){
+        if (showtimeObj.availability[seat]) return false;
+    }
+    return true;
 }
 
 async function updateMovieAvailability(showtimeId, seats){
 
     const showtimesCollection = await showtimes();
-    console.log('here')
+
     let showtimeObj = await showtimesCollection.findOne({showtimeId: showtimeId});
     console.log(showtimeObj);
     for (let seat of seats){

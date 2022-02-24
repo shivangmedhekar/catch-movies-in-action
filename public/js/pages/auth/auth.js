a49d6ef21f9647a8d9158219a4195a709bc4a754
Shivang Medhekar <shivangmedhekar@gmail.com>
2022-02-24 02:17:14 -0500

    better functionality of auth

--- public/js/pages/auth/auth.js
+++ public/js/pages/auth/auth.js
@@ -1,6 +1,59 @@
 (function($) {
-    console.log('here')
+
     navbarUpdate();
+    // const todaysDate = new Date();
+
+    // document.getElementById("dob").max = calenderFormatDate(todaysDate);
+    $('#modalCloseBtn').on("click", function (e){
+        $('#login-form')[0].reset();
+        $('#signup-form')[0].reset();
+        $(".form-control").removeClass('is-invalid');
+        $('.alert').hide();
+    });
+
+    function calenderFormatDate(date){
+        console.log(date.getMonth())
+        let dateString, year, month, day;
+
+        year = date.getFullYear();
+
+        if (date.getMonth() + 1 < 9) month = `0${date.getMonth() + 1}`;
+        else month = date.getMonth() + 1;
+
+        if (date.getDate() < 9) day = `0${date.getDate()}`;
+        else day = date.getDate();
+
+        return `${year}-${month}-${day}`;
+    }
+    $("#login-form").submit(function (event) {
+        $('#signup-success').hide();
+        event.preventDefault();
+
+        var formData = {
+            email: $("#login-email").val(),
+            password: $("#login-password").val(),
+        };
+        let validForm = true;
+        if (formData.email.length === 0 || !ValidateEmail(formData.email)) {
+            validForm = false;
+            $("#login-email").addClass('is-invalid');
+        }
+        else $("#login-email").removeClass('is-invalid');
+
+        if (formData.password.length === 0) {
+            validForm = false;
+            $("#login-password").addClass('is-invalid');
+        }
+        else $("#login-password").removeClass('is-invalid');
+
+        if (validForm) authForm(formData, '/auth/login/', 'login');
+    });
+
+    function ValidateEmail(email)
+    {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) return true;
+        return false;
+    }
 
     $("#signup-form").submit(function (event) {
 
@@ -12,9 +65,10 @@
             phoneNo: $("#phone-no").val(),
             dob: $("#dob").val(),
             password: $("#signup-password").val(),
+            confirmPassword: $("#signup-confirm-password").val(),
         };
 
-        authForm(formData, '/signup/');
+        authForm(formData, '/auth/signup/', 'signup');
         console.log(formData);
     });
 
@@ -22,7 +76,6 @@
         const res = await auth();
 
         if (res.authStatus) {
-            console.log('wtf')
             $('#nav-signup-login').hide();
             $('#nav-account').show();
             $('#nav-account-dropdown').html(`<i class="fas fa-user-circle"></i> Hi ${sessionStorage.getItem('firstName')}`);
@@ -38,7 +91,7 @@
         try {
             const result = await $.ajax({
                 method: 'GET',
-                url: '/auth-status/',
+                url: '/auth/auth-status/',
         })
             if (! result.authStatus) sessionStorage.clear();
             return result
@@ -47,10 +100,18 @@
         }
     }
 
-    async function authForm(formData, url){
+    async function authForm(formData, url, type){
+
         const spinner = '<div class="spinner-border text-light" role="status"><span class="visually-hidden"></span></div>   PLEASE WAIT ...';
-        $('#login-button').attr("disabled", true);
-        $('#login-button').html(spinner);
+
+        if (type === 'login') {
+            $('#login-button').attr("disabled", true);
+            $('#login-button').html(spinner);
+        }
+        else {
+            $('#signup-button').attr("disabled", true);
+            $('#signup-button').html(spinner);
+        }
         try{
             const result = await $.ajax({
                 method: 'POST',
@@ -64,23 +125,48 @@
                     sessionStorage.setItem('authenticated','true');
                     sessionStorage.setItem('firstName', response.firstName);
                     await navbarUpdate();
-                    $('#exampleModal').modal('toggle');
+
+                    if (type === 'login')$('#authModal').modal('toggle');
+                    else {
+                        $('#pills-home-tab').click();
+                        $('#signup-success').html("Your account was created successfully, Login Please");
+                        $('#signup-success').show();
+                    }
+
+                    if (window.location.href.includes('profile')) window.location.replace('/');
+
+                    if (type === 'login') {
+                        $('#login-error').html("");
+                        $('#login-error').hide();
+                    }
+                    else $('#signup-error').html("");
+
+                    $('#login-form')[0].reset();
+                    $('#signup-form')[0].reset();
                 },
             });
-        }catch (e){console.log(e)}
-        $('#login-button').html('LOGIN');
-        $('#login-button').attr("disabled", false);
+        }catch (e){
+            console.log(e)
+            if (type === 'login') {
+                $('#login-error').html(e.responseJSON.error);
+                $('#login-error').show();
+            }
+            else {
+                $('#signup-error').html(e.responseJSON.error);
+                $('#signup-error').show();
+            }
+        }
+
+        if (type === 'login') {
+            $('#login-button').html('LOGIN');
+            $('#login-button').attr("disabled", false);
+        }
+        else {
+            $('#signup-button').html('SIGN UP');
+            $('#signup-button').attr("disabled", false);
+        }
+
     }
-
-    $("#login-form").submit(function (event) {
-
-        event.preventDefault();
-        var formData = {
-            email: $("#login-email").val(),
-            password: $("#login-password").val(),
-        };
-        authForm(formData, '/login/');
-    });
 
     $("#nav-logout").on("click", function (e){ logout();});
 
@@ -88,7 +174,7 @@
         try{
             const result = await $.ajax({
                 method: 'GET',
-                url: '/logout',
+                url: '/auth/logout',
 
                 success: function (response) {
                     sessionStorage.clear();
@@ -99,5 +185,5 @@
         }catch (e){console.log(e)}
     }
 
-
 })(window.jQuery);
+

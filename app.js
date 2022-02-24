338985062f1407f9f12b0371a132ae3c2c77a1cb
Shivang Medhekar <shivangmedhekar@gmail.com>
2022-02-24 15:57:17 -0500

    PORT updated for Heroku

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 configRoutes(app);
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server: http://localhost:3000');
 });

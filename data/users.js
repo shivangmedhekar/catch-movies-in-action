21abf1b4221afaa5256209a45284ae406a7f0090
Shivang Medhekar <shivangmedhekar@gmail.com>
2022-02-24 02:13:50 -0500

    added error handling for data/user.js

--- data/users.js
+++ data/users.js
@@ -8,6 +8,30 @@ const saltRounds = 16;
 
 async function createUser(firstName, lastName, email, phoneNo, dob, password, confirmPassword) {
 
+    /*------------ Error Handling Start -------------*/
+
+    if (!firstName || !lastName || !email || !phoneNo || !dob || !password || !confirmPassword) throw 'Error: All fields are required';
+
+    if (!firstName.trim().length) throw 'Error: first name empty';
+    if (!lastName.trim().length) throw 'Error: last name empty';
+    if (!email.trim().length) throw 'Error: email empty';
+    if (!phoneNo.trim().length) throw 'Error: phone No empty';
+    if (!dob.trim().length) throw 'Error: dob empty';
+    if (!password.trim().length) throw 'Error: password empty';
+    if (!confirmPassword.trim().length) throw 'Error: confirm password empty';
+
+    if(!(firstName.match(/^[a-zA-Z]+$/))) throw 'Error: Invalid first name, only alphabets allowed';
+    if(!(lastName.match(/^[a-zA-Z]+$/))) throw 'Error: Invalid last name, only alphabets allowed';
+    if (!ValidateEmail(email)) throw 'Error: Invalid email';
+    if (!phoneNo.match(/^\d{10}$/)) throw 'Error: Invalid phone number';
+
+    if (!Date.parse(dob)) throw 'Error: Invalid Date of Birth';
+
+    const todaysDate = new Date();
+    if (new Date(dob) - todaysDate > 0) throw 'Error: Invalid Date of Birth';
+
+    if (password !== confirmPassword) throw 'Error: password and confirm password do not match';
+
     email = email.toLowerCase();
     const usersCollection = await users();
     const checkEmail = await usersCollection.findOne({ email: email });
@@ -15,6 +39,7 @@ async function createUser(firstName, lastName, email, phoneNo, dob, password, co
     if (checkEmail) throw "Error: Account with this email exists";
     /*------------ Error Handling End -------------*/
 
+    const dateOfBirth = new Date(dob);
     const hashPassword = await bcrypt.hash(password, saltRounds);
 
     let newUser = {
@@ -22,7 +47,7 @@ async function createUser(firstName, lastName, email, phoneNo, dob, password, co
         lastName: lastName,
         email: email,
         phoneNo: phoneNo,
-        dateOfBirth: dob,
+        dateOfBirth: dateOfBirth,
         password: hashPassword
     }
 
@@ -38,10 +63,22 @@ async function createUser(firstName, lastName, email, phoneNo, dob, password, co
 
 async function checkUser(email, password){
 
+    /*------------ Error Handling Start -------------*/
+
+    if (!email || !password ) throw 'Error: email or password not provided';
+
+    if (!email.trim().length) throw 'Error: email empty';
+    if (!password.trim().length) throw 'Error: password empty';
+
+    if (!ValidateEmail(email)) throw 'Error: Invalid email';
+
+    /*------------ Error Handling End -------------*/
+
+
     const usersCollection = await users();
     const user = await usersCollection.findOne({ email: email });
 
-    if (!user) throw "Error: either the username or password is invalid";
+    if (!user) throw "Error: Either the username or password is invalid";
 
     let comparePasswords = false;
 
@@ -50,7 +87,7 @@ async function checkUser(email, password){
     user._id = String(user._id);
     if (comparePasswords) return {authenticated: true, user: user};
 
-    else throw "Error: either the username or password is invalid";
+    else throw "Error: Either the username or password is invalid";
 }
 
 async function getOrderHistory(userId){
@@ -63,8 +100,9 @@ async function getOrderHistory(userId){
 
 }
 
-function isValid(str){
-    return !/[~`!#@$%\^&*+=\-\[\]\\';.,/{}()[]|\\":<>\?\s]/g.test(str);
+function ValidateEmail(email) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) return true;
+    return false;
 }
 
 module.exports = {
